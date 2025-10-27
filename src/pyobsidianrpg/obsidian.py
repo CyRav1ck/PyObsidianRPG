e409379072f99c0f473c7a2787b15fe7b2a1f6ba
Jensen Dempsey <dev@jnsn.nexus>
2025-10-26 23:01:47 -0600

    got writing to notes working

--- src/pyobsidianrpg/obsidian.py
+++ src/pyobsidianrpg/obsidian.py
@@ -1,27 +1,39 @@
 from pathlib import Path
 import yaml
 
-class Vault():
-    def __init__(self,path):
-        self.path = Path(path)
-        self.notes = [str(p) for p in self.path.rglob('*') if p.is_file() and p.suffix == ".md"]
 
-    def get_note_properties(self):
-        pass
+class Vault():
+    def __init__(self, path):
+        self.path = Path(path)
+        notes = [str(p) for p in self.path.rglob('*') if p.is_file() and p.suffix == ".md"]
+        self.notes = {}
+        for note in notes:
+            self.notes[note] = Note(note)
+
 
 class Note():
-    def __init__(self,path):
+    def __init__(self, path):
         self.path = path
         self.read_properties()
 
     def read_properties(self) -> None:
         self.properties = {}
+        self.content = ""
         with open(self.path, 'r', encoding='utf-8') as f:
             content = f.read()
         # Check if the file starts with YAML front matter delimiters
         if content.startswith('---'):
-            parts = content.split('---', 2) # Split into [empty, yaml_content, markdown_content]
+            parts = content.split('---', 2)  # Split into [empty, yaml_content, markdown_content]
             if len(parts) > 1:
                 yaml_string = parts[1]
+                self.content = parts[2]
                 self.properties = yaml.safe_load(yaml_string)
-        return None # No YAML front matter found
+        return None  # No YAML front matter found
+
+    def write(self, path=None):
+        prop = yaml.dump(self.properties)
+        lines = ['---\n', prop, '---\n', self.content]
+        if path is None:
+            path = self.path
+        with open(path, 'w', encoding='utf-8') as f:
+            f.writelines(lines)
